31fd44dfa19264dd63a2dba93efdbd163a5b91b6
omkar shinde <omkar25shinde@gmail.com>
2022-03-23 00:24:40 +0530

    program

---
+++ Program1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_program.oops
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 51, 1, 3, 4, 98 };
+
+            // get the minimum element
+            Console.WriteLine("Smallest  Element: " + numbers.Min());
+
+            // Max() returns the largest number in array
+            Console.WriteLine("Largest Element: " + numbers.Max());
+
+            Console.ReadLine();
+        }
+    }
+}
